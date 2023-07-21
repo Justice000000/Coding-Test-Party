12e4f3c18649bd80d31091da88c5b214b7ca24f5
domb <woon4910@gmail.com>
2023-07-21 19:25:38 +0900

    [Feat] 20230721 사칙연산...

---
+++ 230721/DongWoon/사칙연산.swift
@@ -0,0 +1,22 @@
+
+
+
+// 두 수의 합
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    return num1 + num2
+}
+
+// 두 수의 차
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    return num1 - num2
+}
+
+// 두 수의 곱
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    return num1 * num2
+}
+
+// 몫 구하기
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    return num1 / num2
+}
