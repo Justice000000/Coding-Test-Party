45e44924d0361e7b78c870c7d8202efa2abc5bab
github-username <dhwoqls1@gmail.com>
2023-07-30 13:26:45 +0900

    jay-230730

---
+++ 230730/jay/jay-230730.playground/Contents.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+//더 크게합치기
+func solution(_ a:Int, _ b:Int) -> Int {
+    let z: Int = Int(String(a) + String(b))!
+    let x: Int = Int(String(b) + String(a))!
+    var result = 0
+    if z > x {
+        result = z
+    } else if z == x {
+        result = z
+    } else {
+        result = x
+    }
+    return result
+}
