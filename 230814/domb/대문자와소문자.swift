77075aa27ba621907acebb910e575661f89b0e96
domb <80871083+DONG-WOON@users.noreply.github.com>
2023-08-14 00:57:02 +0900

    domb - 230814

--- 230814/domb/대문자와소문자.swift
+++
@@ -1,3 +0,0 @@
-func solution(_ my_string:String) -> String {
-    return my_string.map { $0.isUppercase ? $0.lowercased() : $0.uppercased() }.joined()
-}
--- 230814/domb/최댓값만둘기.swift
+++
@@ -1,6 +0,0 @@
-func solution(_ numbers:[Int]) -> Int {
-    var numbers = numbers.sorted()
-    let count = numbers.count
-
-    return max(numbers[0] * numbers[1], numbers[count-1] * numbers[count - 2])
-}
