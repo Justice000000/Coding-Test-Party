ea43536e82dfa016afbac1e8793467781c8df364
domb <80871083+DONG-WOON@users.noreply.github.com>
2023-08-17 23:06:40 +0900

    domb - 230817 (#94)

---
+++ 2308/230817/domb/k의개수.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+func solution(_ i:Int, _ j:Int, _ k:Int) -> Int {
+    return (i...j).map { String($0) }
+                    .joined()
+                    .filter { String($0) == String(k) }
+                    .count
+}
