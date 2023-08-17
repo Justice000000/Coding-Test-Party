257e613ed84a7a000c1a6f250cacecf862170f5a
Taekwon Lee <102043891+andy-archive@users.noreply.github.com>
2023-08-17 13:56:45 +0900

    andy - 230817 (#93)

---
+++ 2308/230817/andy/주사위 게임 2.swift
@@ -0,0 +1,9 @@
+// 프로그래머스 - 주사위 게임 2 https://school.programmers.co.kr/learn/courses/30/lessons/181930
+
+import Foundation
+
+func solution(_ a: Int, _ b: Int, _ c: Int) -> Int {
+    if a == b && b == c { return (a + b + c) * (a*a + b*b + c*c) * (a*a*a + b*b*b + c*c*c) }
+    else if a == b || b == c || c == a { return (a + b + c) * (a*a + b*b + c*c) }
+    else { return a + b + c }
+}
