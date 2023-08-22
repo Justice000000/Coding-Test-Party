bdd8c6a195f547beb7a4e1d791f49b40e0d99e62
Jay <105216574+JaeBeen0725@users.noreply.github.com>
2023-08-23 01:55:57 +0900

    jay - 230822 (#118)
    * test
    
    * jay-230814
    
    * jay-230816
    
    * jay - 230817
    
    * jay - 230821
    
    * jay - 230822

---
+++ 2308/230822/jay/230822.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+//공배수
+func solution(_ number:Int, _ n:Int, _ m:Int) -> Int {
+    
+    if number % n == 0 && number % m == 0{
+        return 1
+    } else {
+         return 0
+    }
+    
+   
+}
