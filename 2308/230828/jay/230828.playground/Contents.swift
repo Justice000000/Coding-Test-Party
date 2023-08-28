e66018fe73c37842928948e08eafbe38bc4b77c0
github-username <dhwoqls1@gmail.com>
2023-08-29 01:58:20 +0900

    jay - 230828

---
+++ 2308/230828/jay/230828.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+//수 조작하기(못품)
+
+func solution(_ numLog:[Int]) -> String {
+    let log: [Int: String] = [1: "w", -1: "s", 10: "d", -10: "a"]
+    return (1..<numLog.count).map { log[numLog[$0] - numLog[$0 - 1]]! }.joined()
+}
+
