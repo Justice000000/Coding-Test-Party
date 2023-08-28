995bced5f104baf5fa0d15fd9455398a6b1b15a6
JongHoon <qnm541@gmail.com>
2023-08-29 01:14:34 +0900

    brick - 230829 (#142)

---
+++ 2308/230828/brick/2839_설탕배달.py
@@ -0,0 +1,15 @@
+from sys import stdin
+
+N = int(stdin.readline())
+
+count = 0
+while N >= 0:
+    if N % 5 == 0:
+        count += N // 5
+        print(count)
+        break
+
+    N -= 3
+    count += 1
+else:
+    print(-1)
