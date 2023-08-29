89242400beb8f265d09346c865f42cca310268b4
RaeBaek <bmp117@naver.com>
2023-08-30 00:40:18 +0900

    230829 코딩테스트 문제풀이

---
+++ 2308/230829/RB/아이스 아메리카노.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  아이스 아메리카노
+//
+//  Created by 백래훈 on 2023/08/30.
+//
+
+import Foundation
+
+func solution(_ money: Int) -> [Int] {
+    
+    return [money / 5500, money % 5500]
+}
+
