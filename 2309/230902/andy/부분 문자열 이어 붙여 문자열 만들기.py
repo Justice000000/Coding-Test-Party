0b6b115e964ba17ac37184b7b9033530979eab18
Taekwon Lee <102043891+andy-archive@users.noreply.github.com>
2023-09-02 23:57:21 +0900

    andy - 230902 (#161)

---
+++ 2309/230902/andy/부분 문자열 이어 붙여 문자열 만들기.py
@@ -0,0 +1,12 @@
+# 부분 문자열 이어 붙여 문자열 만들기
+# https://school.programmers.co.kr/learn/courses/30/lessons/181911
+
+def solution(my_strings, parts):
+
+    answer = ''
+
+    for i in range(len(my_strings)):
+        s, e = parts[i]
+        answer += my_strings[i][s:e+1]
+
+    return answer
