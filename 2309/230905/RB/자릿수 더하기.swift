c623e4e751330cb54bfc239adb7a9ce6b023657d
RaeBaek <bmp117@naver.com>
2023-09-06 01:24:58 +0900

    RB - 230906

---
+++ 2309/230905/RB/자릿수 더하기.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  자릿수 더하기 (2)
+//
+//  Created by 백래훈 on 2023/09/06.
+//
+
+import Foundation
+
+func solution(_ n: Int) -> Int {
+    
+    var answer: Int = 0
+    
+    for i in String(n) {
+        answer += Int(String(i))!
+    }
+    
+    return answer
+}
